3d8f0ee8ae2a266dedda9111dc4d511972fa6f73
Asadbek Shomurodov <110631946+Asadbek16github@users.noreply.github.com>
2023-04-03 09:06:59 +0500

    Boats to Save People --> Medium

---
+++ Problems/881_Boats_to_Save_People.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numRescueBoats(self, people, limit):
+        people.sort()
+        counter = 0
+        l, r = 0, len(people)-1
+
+        while r>=l:
+            if people[r]+people[l]<=limit:
+                l+=1
+            counter+=1
+            r-=1
+        return counter
+    
+print(Solution().numRescueBoats(people = [1,2], limit = 3))
